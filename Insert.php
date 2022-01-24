a60a257cabe20b56d3576baa02eb5975015e8122
Rifzkhy <68432759+Rifzkhy@users.noreply.github.com>
2022-01-24 10:54:12 +0700

    Update Insert.php

--- Insert.php
+++ Insert.php
@@ -3,7 +3,7 @@ include_once '../connection.php';
 
 $kelas = $_POST['kelas'];
 $nama = $_POST['nama'];
-$pass = $_POST['nis'];
+$nis = $_POST['nis'];
 
 $sql = "INSERT INTO $tb(kelas, nama, pass) VALUES ('$kelas','$nama','$pass')";
 	mysqli_query($conn, $sql);
