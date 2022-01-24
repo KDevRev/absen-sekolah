b7747402046d88470f4bc0b657850bac334aa453
Rifzkhy <68432759+Rifzkhy@users.noreply.github.com>
2022-01-24 10:45:49 +0700

    Create Insert.php

---
+++ Insert.php
@@ -0,0 +1,12 @@
+<?php
+include_once '../koneksi.php';
+
+$kelas = $_POST['kelas'];
+$nama = $_POST['nama'];
+$pass = $_POST['pass'];
+
+$sql = "INSERT INTO $tb(kelas, nama, pass) VALUES ('$kelas','$nama','$pass')";
+	mysqli_query($conn, $sql);
+
+header("Location: ../index.php");
+?>
