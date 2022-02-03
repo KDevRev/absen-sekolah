7f044786f814f770ada91460d98bc5d744125991
neku <nekaneka@wkwkwkwk.go.in>
2022-02-03 20:11:41 +0700

    fix: trim mixed-indent-line

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>HALAMAN LOGIN</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HALAMAN LOGIN</title>
 </head>
 <body>
-	<h1>Halaman Login</h1>
-	<form action="" method="post">
-		<ul>
-			<li>
-				<label for="username">Username:</label>
-				<select name="kelas">
-					<option>XIRPL2</option>
-					<option>XIRPL3</option>
-				</select>
+    <h1>Halaman Login</h1>
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username:</label>
+                <select name="kelas">
+                    <option>XIRPL2</option>
+                    <option>XIRPL3</option>
+                </select>
                         </li>
                 </ul>
         </form>
