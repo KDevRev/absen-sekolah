e4358afb8e69200542bd3ad17027a8f29465abb4
Rifzkhy <68432759+Rifzkhy@users.noreply.github.com>
2022-02-06 19:15:25 +0700

    Update view.php

--- fn/view.php
+++ fn/view.php
@@ -1,22 +1,45 @@
 <?php
+// Mengambil koneksi
 include_once '../koneksi.php';
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<?php
-	$sql = "SELECT * FROM $tb WHERE kelas;";
-	$result = mysqli_query($conn, $sql);
-	$resultCheck = mysqli_num_rows($result);
 
-	if ($resultCheck > 0) {
-		while ($row = mysqli_fetch_assoc($result)) {
-			echo $row['$tb'] . "<br>";
+// Cek Koneksi
+if($conn === false){
+	die("ERROR: Tidak dapat konek. " . mysqli_connect_error());
+}
+
+// Attempt select query execution
+$sql = "SELECT * FROM $tb";
+if($result = mysqli_query($conn, $sql)){
+	if(mysqli_num_rows($result) > 0){
+		echo "<link rel='stylesheet' type='text/css' href='style.css'>";
+		echo "<h1>DAFTAR SISWA</h1>";
+		echo "<table border='1' cellpadding='10' cellspacing='0'>";
+		echo "<tr>";
+		echo "<th>No</th>";
+		echo "<th>Kelas</th>";
+		echo "<th>Nama</th>";
+		echo "<th>Nis</th>";
+		echo "<th>Keterangan</th>";
+		echo "</tr>";
+		while($row = mysqli_fetch_array($result)){
+			echo "<tr>";
+			echo "<td>" . $row['no'] . "</td>";
+			echo "<td>" . $row['kelas'] . "</td>";
+			echo "<td>" . $row['nama'] . "</td>";
+			echo "<td>" . $row['nis'] . "</td>";
+			echo "<td>" . $row['ket'] . "</td>";
+			echo "</tr>";
 		}
+		echo "</table>";
+        // Cek Database ada/tidak
+		mysqli_free_result($result);
+	} else{
+		echo "Tidak ada data ditemukan";
 	}
-	?>
-</body>
-</html>
+} else{
+	echo "ERROR: Tidak dapat di execute $sql. " . mysqli_error($conn);
+}
+
+// Menutup Koneksi
+mysqli_close($conn);
+?>
