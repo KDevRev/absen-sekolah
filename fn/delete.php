f0385406f05f14d8e405b336b7b833296f3ba870
nekaru <32153003+Ncode2014@users.noreply.github.com>
2022-02-08 08:21:57 +0700

    rewrite: function delete

--- fn/delete.php
+++ fn/delete.php
@@ -1,5 +1,11 @@
 <?php
 include_once '../koneksi.php';
 
-$sql = "DELETE FROM $tb WHERE id=3"
+// Untuk Bagian Nama
+$nama = $_GET['nama'];
+// Bagian query Sql untuk cari data
+$sql = "DELETE FROM $tb WHERE nama='$nama'";
+mysqli_query($conn, $sql);
+// mengalihkan ke index.php
+header("location:../index.php")
 ?>
